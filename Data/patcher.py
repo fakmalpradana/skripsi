c4339c268e70c0ae22e97642d841ff994753b851
fakmalpradana <fakmalpradana@gmail.com>
2023-03-01 12:21:55 +0700

    menambahkan patch segmented

--- Data/patcher.py
+++ Data/patcher.py
@@ -2,7 +2,7 @@ import numpy as np
 from patchify import patchify as p
 import cv2
 
-raw_img = cv2.imread('HillShade_30.png')
+raw_img = cv2.imread('Segmented_aoi.png')
 # raw_mask = cv2.imread('mask.png')
 
 # for img in range(raw_img.shape[0]):
@@ -27,7 +27,7 @@ patches_img = p(raw_img, (512, 512, 3), step=(448))
 for i in range(patches_img.shape[0]):
     for j in range(patches_img.shape[1]):
         single_patch_img = patches_img[i, j, 0, :, :, :]
-        if not cv2.imwrite('hsd_patch/' + 'hillshade_' + str(i) + '_' +str(j)+'.png', single_patch_img):
+        if not cv2.imwrite('sgm_patch/' + 'segmented_' + str(i) + '_' +str(j)+'.png', single_patch_img):
             raise Exception("Gaiso nulis image")
 
 # for i in range(patches_mask.shape[0]):
